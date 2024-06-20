705b2585b5a8c76151d2ac3156b6913effc630df
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-06-20 13:49:09 +0300

    made changes

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    firstname= models.CharField(max_length=20)
+    lastname= models.CharField(max_length=20)
+    email= models.EmailField()
+    teachers_id= models.PositiveSmallIntegerField()
+    phonenumber=models.CharField(max_length=20)
+    subject_specialization=models.CharField(max_length=28)
+    years_of_experience=models.IntegerField()
+    office_hours= models.IntegerField()
+    biography=models.TextField(max_length=50)
+    course_taught=models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
